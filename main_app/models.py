ec6c53c1639ca328c4a9fb8bf71e77aa2fc51154
JMoney-216 <jdm216@gmail.com>
2019-12-17 22:24:54 -0600

    Adds mileage field to motorcycle model Adds some functionality to dispay if oil change is due, but commented out for now
     Changes to be committed:
    	new file:   main_app/migrations/0003_motorcycle_mileage.py
    	modified:   main_app/models.py
    	modified:   main_app/templates/motorcycles/detail.html

--- main_app/models.py
+++ main_app/models.py
@@ -7,12 +7,17 @@ class Acc(models.Model):
   size = models.CharField(max_length=100)
   color = models.CharField(max_length=100)
 
+# class oil_due(model.Models):
+#   return self.
+
+
 class Motorcycle(models.Model):
   brand = models.CharField(max_length=100)
   model = models.CharField(max_length=100)
   style = models.CharField(max_length=100)
   year = models.IntegerField()
   color = models.CharField(max_length=100)
+  mileage = models.IntegerField(default=100)
   accs = models.ManyToManyField(Acc)
   
   def __str__(self):
