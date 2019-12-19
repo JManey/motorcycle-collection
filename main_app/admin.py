1482be7bd8a533bcf5dba346400eefdbe9846e33
JMoney-216 <jdm216@gmail.com>
2019-12-18 19:25:48 -0600

    adds Photos to admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Motorcycle, Acc
+from .models import Motorcycle, Acc, Photo
 
 # Register your models here.
 admin.site.register(Motorcycle)
 admin.site.register(Acc)
+admin.site.register(Photo)
